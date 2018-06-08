d82ace5a1c782e9dafc33a343dccc7c11072d488
BENEQ <robertengel@wp.pl>
2018-06-08 23:43:40 +0200

    dodanie info zalogowanego usera (avatar, login i czy jest aktywny)

--- app/src/main/java/com/closechat/closechat/UserList.java
+++ app/src/main/java/com/closechat/closechat/UserList.java
@@ -2,16 +2,16 @@ package com.closechat.closechat;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class UserList extends Activity {
 
-
+    int[] IMAGES = {R.drawable.avatar, R.drawable.avatar, R.drawable.avatar};
     String[] NAMES = {"user1", "koziolekmatolek", "agent BOLEK"};
     String[] DESCRIPTION = {"jakiś nob", "kolo z Pacanowa", "a ten był prezydentem"};
 
@@ -20,9 +20,16 @@ public class UserList extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_list);
 
-        ListView userList=(ListView)findViewById(R.id.ListView);
 
-        UsserItem user=new UsserItem();
+        TextView textView_login = (TextView) findViewById(R.id.textView_login);
+        textView_login.setText("grzegorz brzęczyszczykiewicz");
+
+        ImageView imageView_avatar=(ImageView)findViewById(R.id.imageView_user_login);
+        imageView_avatar.setImageResource(IMAGES[0]);
+
+        ListView userList = (ListView) findViewById(R.id.ListView);
+
+        UsserItem user = new UsserItem();
         userList.setAdapter(user);
     }
 
@@ -45,11 +52,13 @@ public class UserList extends Activity {
 
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
-            view = getLayoutInflater().inflate(R.layout.user_item,null);
+            view = getLayoutInflater().inflate(R.layout.user_item, null);
 
-            TextView textView_name=(TextView)view.findViewById(R.id.textView_name);
-            TextView textView_description=(TextView)view.findViewById(R.id.textView_description);
+            ImageView imageView_avatar=(ImageView)view.findViewById(R.id.imageView_avatar);
+            TextView textView_name = (TextView) view.findViewById(R.id.textView_name);
+            TextView textView_description = (TextView) view.findViewById(R.id.textView_description);
 
+            imageView_avatar.setImageResource(IMAGES[i]);
             textView_name.setText(NAMES[i]);
             textView_description.setText(DESCRIPTION[i]);
             return view;
