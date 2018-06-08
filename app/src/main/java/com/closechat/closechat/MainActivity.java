8ee060b43f68ab90fad48cebdb82296a8e4e5dad
ThisIsMyWay <skaczacymuchomor@gmail.com>
2018-06-08 21:38:47 +0200

    added login screen

--- app/src/main/java/com/closechat/closechat/MainActivity.java
+++ app/src/main/java/com/closechat/closechat/MainActivity.java
@@ -1,13 +1,43 @@
 package com.closechat.closechat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+
+    EditText loginEditText;
+    Button logInActionBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connectWithView();
+        addActionToViews();
     }
+
+    private void connectWithView() {
+        loginEditText =  this.findViewById(R.id.login_edit_text);
+        logInActionBtn = this.findViewById(R.id.log_in_btn);
+    }
+
+    private void addActionToViews() {
+
+        logInActionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (loginEditText.getText().toString().trim().length() > 0) {
+                    Toast.makeText( MainActivity.this, "not empty", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText( MainActivity.this, "empty", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
 }
